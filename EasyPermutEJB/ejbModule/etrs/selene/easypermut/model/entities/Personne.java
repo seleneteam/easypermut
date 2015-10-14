abca171badd4a2dd0656ae33118df1d56927bdbd
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-14 09:45:00 +0200

    a

--- EasyPermutEJB/ejbModule/etrs/selene/easypermut/model/entities/Personne.java
+++ EasyPermutEJB/ejbModule/etrs/selene/easypermut/model/entities/Personne.java
@@ -1,10 +1,12 @@
 package etrs.selene.easypermut.model.entities;
 
 import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,19 +20,20 @@ import etrs.selene.easypermut.model.commons.GeneratedUUID;
 @EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "personne")
-public class Personne implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedUUID
-    @Column(name = "id")
-    String id;
-
-    @Column(name = "nom")
-    String nom;
-
-    @Column(name = "prenom")
-    String prenom;
-
+public class Personne implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedUUID
+	@Column(name = "id")
+	String id;
+	
+	@Column(name = "nom")
+	String nom;
+	
+	@Column(name = "prenom")
+	String prenom;
+	
 }

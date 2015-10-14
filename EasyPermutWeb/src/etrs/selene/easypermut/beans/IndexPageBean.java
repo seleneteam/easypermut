da76a475c270bcd821eff0f13b6c51e09dda5e50
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-14 09:25:55 +0200

    modif orthographe

--- EasyPermutWeb/src/etrs/selene/easypermut/beans/IndexPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/IndexPageBean.java
@@ -2,10 +2,12 @@ package etrs.selene.easypermut.beans;
 
 import java.io.Serializable;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,28 +21,32 @@ import etrs.selene.easypermut.utils.JsfUtils;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @ViewScoped
-public class IndexPageBean implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @Inject
-    PersonneSession facadePersonne;
-
-    Personne personne;
-
-    @PostConstruct
-    public void init() {
-        personne = facadePersonne.newInstance();
-    }
-
-    public void ajouterPersonne() {
-        facadePersonne.create(personne);
-        JsfUtils.sendMessage(String.format("La personne %s a été crée", personne.getNom()));
-        personne = facadePersonne.newInstance();
-    }
-
-    public List<Personne> getLstPersonnes() {
-        return facadePersonne.readAll();
-    }
-
+public class IndexPageBean implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	PersonneSession facadePersonne;
+	
+	Personne personne;
+	
+	@PostConstruct
+	public void init()
+	{
+		this.personne = this.facadePersonne.newInstance();
+	}
+	
+	public void ajouterPersonne()
+	{
+		this.facadePersonne.create(this.personne);
+		JsfUtils.sendMessage(String.format("La personne %s a été créée", this.personne.getNom()));
+		this.personne = this.facadePersonne.newInstance();
+	}
+	
+	public List<Personne> getLstPersonnes()
+	{
+		return this.facadePersonne.readAll();
+	}
+	
 }

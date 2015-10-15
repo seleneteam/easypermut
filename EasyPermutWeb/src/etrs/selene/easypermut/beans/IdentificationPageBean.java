8cde9b2fa0df50a69169aa394c33ba3c03084cb9
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-15 15:40:37 +0200

    suppression jsfutils,genericconverter

--- EasyPermutWeb/src/etrs/selene/easypermut/beans/IdentificationPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/IdentificationPageBean.java
@@ -2,83 +2,94 @@ package etrs.selene.easypermut.beans;
 
 import java.io.Serializable;
 import java.net.URI;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import net.entetrs.commons.jsf.JsfUtils;
 import etrs.selene.easypermut.model.entities.Utilisateur;
 import etrs.selene.easypermut.model.sessions.UtilisateurSession;
-import etrs.selene.easypermut.utils.JsfUtils;
 
 @Named
 @RequestScoped
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class IdentificationPageBean implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * L'utilisateur dont l'identifiant anudef est present dans le formulaire.
-     */
-    Utilisateur u;
-
-    /**
-     * URI de redirection en cas d'erreur.
-     */
-    URI uri;
-
-    @Inject
-    UtilisateurSession facadeUtilisateur;
-
-    /**
-     * Methode de post contruction. Instancie un nouvel utilisateur.
-     */
-    @PostConstruct
-    public void init() {
-        this.u = this.facadeUtilisateur.newInstance();
-    }
-
-    /**
-     * Methude de mise en FlashScope de l'utilisateur.
-     * 
-     * @param u
-     *            L'utilisateur a mettre dans le FlashScope.
-     */
-    public void flashUtilisateur(Utilisateur u) {
-        JsfUtils.putInFlashScope("_utilisateur", u);
-    }
-
-    /**
-     * Methode de connexion. Si l'utilisateur existe et que il est validé (voir
-     * {@link Utilisateur#getEstValide()}) redirige vers la page d'accueil de
-     * l'application. Si l'utilisateur existe, mais qu'il n'est pas validé,
-     * redirige vers une page d'attente. Si l'utilisateur n'existe pas, l'ajoute
-     * dans la BDD et met son status a non valide.
-     * 
-     * @return L'URL de redirection en fonction du cas.
-     */
-    public String connexion() {
-        String redirectedUrl;
-
-        Utilisateur utilisateurATester = this.facadeUtilisateur.searchFirstResult("identifiantAnudef", u.getIdentifiantAnudef());
-
-        if (utilisateurATester != null && utilisateurATester.getEstValide() == true) {
-            flashUtilisateur(utilisateurATester);
-            redirectedUrl = "";
-        } else if (utilisateurATester != null && utilisateurATester.getEstValide() == false) {
-            redirectedUrl = "";
-        } else {
-            utilisateurATester.setEstValide(false);
-            this.facadeUtilisateur.create(utilisateurATester);
-            redirectedUrl = "";
-        }
-
-        return redirectedUrl;
-    }
+public class IdentificationPageBean implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * L'utilisateur dont l'identifiant anudef est present dans le formulaire.
+	 */
+	Utilisateur u;
+	
+	/**
+	 * URI de redirection en cas d'erreur.
+	 */
+	URI uri;
+	
+	@Inject
+	UtilisateurSession facadeUtilisateur;
+	
+	/**
+	 * Methode de post contruction. Instancie un nouvel utilisateur.
+	 */
+	@PostConstruct
+	public void init()
+	{
+		this.u = this.facadeUtilisateur.newInstance();
+	}
+	
+	/**
+	 * Methude de mise en FlashScope de l'utilisateur.
+	 * 
+	 * @param u
+	 *            L'utilisateur a mettre dans le FlashScope.
+	 */
+	public void flashUtilisateur(final Utilisateur u)
+	{
+		JsfUtils.putInFlashScope("_utilisateur", u);
+	}
+	
+	/**
+	 * Methode de connexion. Si l'utilisateur existe et que il est validé (voir
+	 * {@link Utilisateur#getEstValide()}) redirige vers la page d'accueil de
+	 * l'application. Si l'utilisateur existe, mais qu'il n'est pas validé,
+	 * redirige vers une page d'attente. Si l'utilisateur n'existe pas, l'ajoute
+	 * dans la BDD et met son status a non valide.
+	 * 
+	 * @return L'URL de redirection en fonction du cas.
+	 */
+	public String connexion()
+	{
+		String redirectedUrl;
+		
+		Utilisateur utilisateurATester = this.facadeUtilisateur.searchFirstResult("identifiantAnudef", this.u.getIdentifiantAnudef());
+		
+		if (utilisateurATester != null && utilisateurATester.getEstValide() == true)
+		{
+			this.flashUtilisateur(utilisateurATester);
+			redirectedUrl = "";
+		}
+		else if (utilisateurATester != null && utilisateurATester.getEstValide() == false)
+		{
+			redirectedUrl = "";
+		}
+		else
+		{
+			utilisateurATester.setEstValide(false);
+			this.facadeUtilisateur.create(utilisateurATester);
+			redirectedUrl = "";
+		}
+		
+		return redirectedUrl;
+	}
 }

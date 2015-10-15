1699566e1e136a1297dccbf3a86b6a47c9693da7
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-15 15:47:38 +0200

    ajout des valus à connexion.xhtml

--- EasyPermutWeb/src/etrs/selene/easypermut/beans/IdentificationPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/IdentificationPageBean.java
@@ -23,57 +23,57 @@ import etrs.selene.easypermut.model.sessions.UtilisateurSession;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class IdentificationPageBean implements Serializable
 {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * L'utilisateur dont l'identifiant anudef est present dans le formulaire.
 	 */
-	Utilisateur u;
-	
+	Utilisateur utilisateur;
+
 	/**
 	 * URI de redirection en cas d'erreur.
 	 */
 	URI uri;
-	
+
 	@Inject
 	UtilisateurSession facadeUtilisateur;
-	
+
 	/**
 	 * Methode de post contruction. Instancie un nouvel utilisateur.
 	 */
 	@PostConstruct
 	public void init()
 	{
-		this.u = this.facadeUtilisateur.newInstance();
+		this.utilisateur = this.facadeUtilisateur.newInstance();
 	}
-	
+
 	/**
 	 * Methude de mise en FlashScope de l'utilisateur.
-	 * 
+	 *
 	 * @param u
 	 *            L'utilisateur a mettre dans le FlashScope.
 	 */
-	public void flashUtilisateur(final Utilisateur u)
+	public void flashUtilisateur(final Utilisateur utilisateur)
 	{
-		JsfUtils.putInFlashScope("_utilisateur", u);
+		JsfUtils.putInFlashScope("_utilisateur", utilisateur);
 	}
-	
+
 	/**
 	 * Methode de connexion. Si l'utilisateur existe et que il est validé (voir
 	 * {@link Utilisateur#getEstValide()}) redirige vers la page d'accueil de
 	 * l'application. Si l'utilisateur existe, mais qu'il n'est pas validé,
 	 * redirige vers une page d'attente. Si l'utilisateur n'existe pas, l'ajoute
 	 * dans la BDD et met son status a non valide.
-	 * 
+	 *
 	 * @return L'URL de redirection en fonction du cas.
 	 */
 	public String connexion()
 	{
 		String redirectedUrl;
-		
-		Utilisateur utilisateurATester = this.facadeUtilisateur.searchFirstResult("identifiantAnudef", this.u.getIdentifiantAnudef());
-		
+
+		Utilisateur utilisateurATester = this.facadeUtilisateur.searchFirstResult("identifiantAnudef", this.utilisateur.getIdentifiantAnudef());
+
 		if (utilisateurATester != null && utilisateurATester.getEstValide() == true)
 		{
 			this.flashUtilisateur(utilisateurATester);
@@ -89,7 +89,7 @@ public class IdentificationPageBean implements Serializable
 			this.facadeUtilisateur.create(utilisateurATester);
 			redirectedUrl = "";
 		}
-		
+
 		return redirectedUrl;
 	}
 }

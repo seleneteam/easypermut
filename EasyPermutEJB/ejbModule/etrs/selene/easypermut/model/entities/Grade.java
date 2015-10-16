f5a7a7d18db3bf4cb0f615f5d4453e8113a32963
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-16 11:27:48 +0200

    ajout de consultation et creation

--- EasyPermutEJB/ejbModule/etrs/selene/easypermut/model/entities/Grade.java
+++ EasyPermutEJB/ejbModule/etrs/selene/easypermut/model/entities/Grade.java
@@ -1,38 +1,40 @@
 package etrs.selene.easypermut.model.entities;
 
 import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-import org.hibernate.validator.constraints.Length;
 import etrs.selene.easypermut.model.commons.AbstractEntity;
 
 /**
  * Classe représentant l'entitée Grade.
- * 
- * @author SGT Mora Leo
  *
+ * @author SGT Mora Leo
  */
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @Entity
 @Table(name = "GRADE")
-public class Grade extends AbstractEntity implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * Libelle du grade.
-     */
-    @Column(name = "grade")
-    @Length(min = 3, max = 3)
-    @Getter
-    @Setter
-    String grade;
-
+public class Grade extends AbstractEntity implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Libelle du grade.
+	 */
+	@Column(name = "grade")
+	@NotNull
+	@Getter
+	@Setter
+	String grade;
+	
 }

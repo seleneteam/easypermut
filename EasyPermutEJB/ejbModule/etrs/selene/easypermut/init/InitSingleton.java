8f540dbcef596d7b8288e22914e8bcd852d3fe43
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-17 11:50:15 +0200

    création d'une page validationEnCours et modification de la page de creation de demande

--- EasyPermutEJB/ejbModule/etrs/selene/easypermut/init/InitSingleton.java
+++ EasyPermutEJB/ejbModule/etrs/selene/easypermut/init/InitSingleton.java
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+
 import lombok.extern.apachecommons.CommonsLog;
 import etrs.selene.easypermut.model.entities.Grade;
 import etrs.selene.easypermut.model.entities.Poste;
@@ -23,206 +24,234 @@ import etrs.selene.easypermut.model.sessions.ZMRSession;
 @Singleton
 @Startup
 @CommonsLog
-public class InitSingleton {
-    /*
-     * @Inject private AvisSession facadeAvis;
-     * 
-     * @Inject private DemandePermutationSession facadeDemandePermutation;
-     */
+public class InitSingleton
+{
+	/*
+	 * @Inject private AvisSession facadeAvis;
+	 * @Inject private DemandePermutationSession facadeDemandePermutation;
+	 */
 
-    @Inject
-    private GradeSession facadeGrade;
+	@Inject
+	private GradeSession facadeGrade;
 
-    @Inject
-    private PosteSession facadePoste;
+	@Inject
+	private PosteSession facadePoste;
 
-    @Inject
-    private SpecialiteSession facadeSpecialite;
+	@Inject
+	private SpecialiteSession facadeSpecialite;
 
-    @Inject
-    private UniteSession facadeUnite;
+	@Inject
+	private UniteSession facadeUnite;
 
-    @Inject
-    private UtilisateurSession facadeUtilisateur;
+	@Inject
+	private UtilisateurSession facadeUtilisateur;
 
-    @Inject
-    private VilleSession facadeVille;
+	@Inject
+	private VilleSession facadeVille;
 
-    @Inject
-    private ZMRSession facadeZMR;
+	@Inject
+	private ZMRSession facadeZMR;
 
-    String[] tabGrade = { "caporal", "caporal-chef", "sergent", "adjudant" };
-    String[] tabPoste = { "mecanicien", "electrotechnichien", "previsionniste" };
-    String[] tabNomSpecialite = { "programmeur", "admin-reseaux", "communication" };
-    String[] tabSpecialite = { "8300", "8200", "8100" };
-    String[] tabUnite = { "edcm", "esta", "dsi" };
-    String[] tabUtilisateur = { "didier", "serge", "cecile" };
-    String[] tabVille = { "paris", "rennes", "bordeaux" };
-    String[] tabZMR = { "paris", "rennes", "bordeaux" };
+	String[] tabGrade = { "caporal", "caporal-chef", "sergent", "adjudant" };
+	String[] tabPoste = { "mecanicien", "electrotechnichien", "previsionniste" };
+	String[] tabNomSpecialite = { "programmeur", "admin-reseaux", "communication" };
+	String[] tabSpecialite = { "8300", "8200", "8100" };
+	String[] tabUnite = { "edcm", "esta", "dsi" };
+	String[] tabUtilisateur = { "didier", "serge", "cecile" };
+	String[] tabVille = { "paris", "rennes", "bordeaux" };
+	String[] tabZMR = { "paris", "rennes", "bordeaux" };
 
-    @PostConstruct
-    public void init() {
-        log.info("Vérification de la base de donnée.....");
+	@PostConstruct
+	public void init()
+	{
+		log.info("Vérification de la base de donnée.....");
 
-        /*
-         * Ajout de grades.
-         */
-        if (this.facadeGrade.readAll().isEmpty()) {
-            for (String string : this.tabGrade) {
-                Grade grade = this.facadeGrade.newInstance();
-                grade.setGrade(string);
-                this.facadeGrade.create(grade);
-                log.info("Grade " + string + " créé");
-            }
-        } else {
-            log.info("La table grade contient déja des éléments");
-        }
+		/*
+		 * Ajout de grades.
+		 */
+		if (this.facadeGrade.readAll().isEmpty())
+		{
+			for (String string : this.tabGrade)
+			{
+				Grade grade = this.facadeGrade.newInstance();
+				grade.setGrade(string);
+				this.facadeGrade.create(grade);
+				log.info("Grade " + string + " créé");
+			}
+		}
+		else
+		{
+			log.info("La table grade contient déja des éléments");
+		}
 
-        /*
-         * Ajout de ZMR.
-         */
-        if (this.facadeZMR.readAll().isEmpty()) {
+		/*
+		 * Ajout de ZMR.
+		 */
+		if (this.facadeZMR.readAll().isEmpty())
+		{
 
-            for (String string : this.tabZMR) {
-                ZMR zmr = this.facadeZMR.newInstance();
-                zmr.setLibelle(string);
-                this.facadeZMR.create(zmr);
-                log.info("ZMR " + string + " créé");
-            }
-        } else {
-            log.info("La table ville contient déja des éléments");
-        }
+			for (String string : this.tabZMR)
+			{
+				ZMR zmr = this.facadeZMR.newInstance();
+				zmr.setLibelle(string);
+				this.facadeZMR.create(zmr);
+				log.info("ZMR " + string + " créé");
+			}
+		}
+		else
+		{
+			log.info("La table ville contient déja des éléments");
+		}
 
-        /*
-         * Ajout de villes.
-         */
-        if (this.facadeVille.readAll().isEmpty()) {
-            int index = 0;
+		/*
+		 * Ajout de villes.
+		 */
+		if (this.facadeVille.readAll().isEmpty())
+		{
+			int index = 0;
 
-            for (String string : this.tabVille) {
-                Ville ville = this.facadeVille.newInstance();
-                ville.setNom(string);
-                ville.setZmr(this.facadeZMR.searchFirstResult("libelle", this.tabZMR[index]));
-                this.facadeVille.create(ville);
-                log.info("Ville " + string + " créé");
-                index++;
-            }
-        } else {
-            log.info("La table ville contient déja des éléments");
-        }
+			for (String string : this.tabVille)
+			{
+				Ville ville = this.facadeVille.newInstance();
+				ville.setNom(string);
+				ville.setZmr(this.facadeZMR.searchFirstResult("libelle", this.tabZMR[index]));
+				this.facadeVille.create(ville);
+				log.info("Ville " + string + " créé");
+				index++;
+			}
+		}
+		else
+		{
+			log.info("La table ville contient déja des éléments");
+		}
 
-        /*
-         * Ajout d'unités.
-         */
-        if (this.facadeUnite.readAll().isEmpty()) {
-            int index = 0;
+		/*
+		 * Ajout d'unités.
+		 */
+		if (this.facadeUnite.readAll().isEmpty())
+		{
+			int index = 0;
 
-            for (String string : this.tabUnite) {
-                Unite unite = this.facadeUnite.newInstance();
-                unite.setLibelle(string);
-                unite.setVille(this.facadeVille.searchFirstResult("nom", this.tabVille[index]));
-                this.facadeUnite.create(unite);
-                log.info("Unite " + string + " créé");
-                index++;
-            }
-        } else {
-            log.info("La table unite contient déja des éléments");
-        }
+			for (String string : this.tabUnite)
+			{
+				Unite unite = this.facadeUnite.newInstance();
+				unite.setLibelle(string);
+				unite.setVille(this.facadeVille.searchFirstResult("nom", this.tabVille[index]));
+				this.facadeUnite.create(unite);
+				log.info("Unite " + string + " créé");
+				index++;
+			}
+		}
+		else
+		{
+			log.info("La table unite contient déja des éléments");
+		}
 
-        /*
-         * Ajout de postes.
-         */
-        if (this.facadePoste.readAll().isEmpty()) {
-            int index = 0;
+		/*
+		 * Ajout de postes.
+		 */
+		if (this.facadePoste.readAll().isEmpty())
+		{
+			int index = 0;
 
-            for (String string : this.tabPoste) {
-                Poste poste = this.facadePoste.newInstance();
-                poste.setLibelle(string);
-                poste.setUnite(this.facadeUnite.searchFirstResult("libelle", this.tabUnite[index]));
-                this.facadePoste.create(poste);
-                log.info("Poste " + string + " créé");
-                index++;
-            }
-        } else {
-            log.info("La table poste contient déja des éléments");
-        }
+			for (String string : this.tabPoste)
+			{
+				Poste poste = this.facadePoste.newInstance();
+				poste.setLibelle(string);
+				poste.setUnite(this.facadeUnite.searchFirstResult("libelle", this.tabUnite[index]));
+				this.facadePoste.create(poste);
+				log.info("Poste " + string + " créé");
+				index++;
+			}
+		}
+		else
+		{
+			log.info("La table poste contient déja des éléments");
+		}
 
-        /*
-         * Ajout de specialite.
-         */
-        if (this.facadeSpecialite.readAll().isEmpty()) {
-            int index = 0;
+		/*
+		 * Ajout de specialite.
+		 */
+		if (this.facadeSpecialite.readAll().isEmpty())
+		{
+			int index = 0;
 
-            for (String string : this.tabNomSpecialite) {
-                Specialite specialite = this.facadeSpecialite.newInstance();
-                specialite.setLibelle(string);
-                specialite.setNumeroSpe(this.tabSpecialite[index]);
-                this.facadeSpecialite.create(specialite);
-                log.info("Specialite " + string + " créé");
-                index++;
-            }
-        } else {
-            log.info("La table specialite contient déja des éléments");
-        }
+			for (String string : this.tabNomSpecialite)
+			{
+				Specialite specialite = this.facadeSpecialite.newInstance();
+				specialite.setLibelle(string);
+				specialite.setNumeroSpe(this.tabSpecialite[index]);
+				this.facadeSpecialite.create(specialite);
+				log.info("Specialite " + string + " créé");
+				index++;
+			}
+		}
+		else
+		{
+			log.info("La table specialite contient déja des éléments");
+		}
 
-        /*
-         * Ajout d'utilisateurs.
-         */
-        if (this.facadeUtilisateur.readAll().isEmpty()) {
-            int index = 0;
+		/*
+		 * Ajout d'utilisateurs.
+		 */
+		if (this.facadeUtilisateur.readAll().isEmpty())
+		{
+			int index = 0;
 
-            Utilisateur utilisateur = this.facadeUtilisateur.newInstance();
-            utilisateur.setEstValide(true);
-            utilisateur.setEstInteresse(false);
-            utilisateur.setInformationsValide(true);
-            utilisateur.setGrade(this.facadeGrade.searchFirstResult("grade", this.tabGrade[index]));
-            utilisateur.setIdentifiantAnudef(this.tabUtilisateur[index]);
-            utilisateur.setMail("random@gouv.fr");
-            utilisateur.setNia("AL42698");
-            utilisateur.setNom(this.tabUtilisateur[index]);
-            utilisateur.setPoste(this.facadePoste.searchFirstResult("libelle", this.tabPoste[index]));
-            utilisateur.setPrenom(this.tabUtilisateur[index]);
-            utilisateur.setSpecialite(this.facadeSpecialite.searchFirstResult("libelle", this.tabSpecialite[index]));
-            this.facadeUtilisateur.create(utilisateur);
-            log.info("Utilisateur " + this.tabUtilisateur[index] + " créé");
-            index++;
+			Utilisateur utilisateur = this.facadeUtilisateur.newInstance();
+			utilisateur.setEstValide(true);
+			utilisateur.setEstInteresse(false);
+			utilisateur.setInformationsValide(true);
+			utilisateur.setGrade(this.facadeGrade.searchFirstResult("grade", this.tabGrade[index]));
+			utilisateur.setIdentifiantAnudef(this.tabUtilisateur[index]);
+			utilisateur.setMail("random@gouv.fr");
+			utilisateur.setNia("AL42698");
+			utilisateur.setNom(this.tabUtilisateur[index]);
+			utilisateur.setPoste(this.facadePoste.searchFirstResult("libelle", this.tabPoste[index]));
+			utilisateur.setPrenom(this.tabUtilisateur[index]);
+			utilisateur.setSpecialite(this.facadeSpecialite.searchFirstResult("libelle", this.tabNomSpecialite[index]));
+			this.facadeUtilisateur.create(utilisateur);
+			log.info("Utilisateur " + this.tabUtilisateur[index] + " créé");
+			index++;
 
-            Utilisateur utilisateur2 = this.facadeUtilisateur.newInstance();
-            utilisateur2.setEstValide(false);
-            utilisateur2.setInformationsValide(true);
-            utilisateur2.setEstInteresse(false);
-            utilisateur2.setIdentifiantAnudef(this.tabUtilisateur[index]);
-            this.facadeUtilisateur.create(utilisateur2);
-            log.info("Utilisateur " + this.tabUtilisateur[index] + " créé");
-            index++;
+			Utilisateur utilisateur2 = this.facadeUtilisateur.newInstance();
+			utilisateur2.setEstValide(false);
+			utilisateur2.setInformationsValide(true);
+			utilisateur2.setEstInteresse(false);
+			utilisateur2.setIdentifiantAnudef(this.tabUtilisateur[index]);
+			this.facadeUtilisateur.create(utilisateur2);
+			log.info("Utilisateur " + this.tabUtilisateur[index] + " créé");
+			index++;
 
-            Utilisateur utilisateur3 = this.facadeUtilisateur.newInstance();
-            utilisateur3.setEstValide(false);
-            utilisateur3.setInformationsValide(true);
-            utilisateur3.setEstInteresse(false);
-            utilisateur3.setIdentifiantAnudef(this.tabUtilisateur[index]);
-            this.facadeUtilisateur.create(utilisateur3);
-            log.info("Utilisateur " + this.tabUtilisateur[index] + " créé");
+			Utilisateur utilisateur3 = this.facadeUtilisateur.newInstance();
+			utilisateur3.setEstValide(false);
+			utilisateur3.setInformationsValide(true);
+			utilisateur3.setEstInteresse(false);
+			utilisateur3.setIdentifiantAnudef(this.tabUtilisateur[index]);
+			this.facadeUtilisateur.create(utilisateur3);
+			log.info("Utilisateur " + this.tabUtilisateur[index] + " créé");
 
-            Utilisateur utilisateur4 = this.facadeUtilisateur.newInstance();
-            utilisateur4.setEstValide(false);
-            utilisateur4.setEstInteresse(false);
-            utilisateur4.setInformationsValide(true);
-            utilisateur4.setIdentifiantAnudef("test.trynet.21539");
-            this.facadeUtilisateur.create(utilisateur4);
-            log.info("Utilisateur 4 créé");
+			Utilisateur utilisateur4 = this.facadeUtilisateur.newInstance();
+			utilisateur4.setEstValide(false);
+			utilisateur4.setEstInteresse(false);
+			utilisateur4.setInformationsValide(true);
+			utilisateur4.setIdentifiantAnudef("test.trynet.21539");
+			this.facadeUtilisateur.create(utilisateur4);
+			log.info("Utilisateur 4 créé");
 
-            Utilisateur utilisateur5 = this.facadeUtilisateur.newInstance();
-            utilisateur5.setEstValide(false);
-            utilisateur5.setInformationsValide(true);
-            utilisateur5.setEstInteresse(false);
-            utilisateur5.setIdentifiantAnudef("wotan.yoshin.56924");
-            this.facadeUtilisateur.create(utilisateur5);
-            log.info("Utilisateur 5 créé");
+			Utilisateur utilisateur5 = this.facadeUtilisateur.newInstance();
+			utilisateur5.setEstValide(false);
+			utilisateur5.setInformationsValide(true);
+			utilisateur5.setEstInteresse(false);
+			utilisateur5.setIdentifiantAnudef("wotan.yoshin.56924");
+			this.facadeUtilisateur.create(utilisateur5);
+			log.info("Utilisateur 5 créé");
 
-        } else {
-            log.info("La table utilisateur contient déja des éléments");
-        }
+		}
+		else
+		{
+			log.info("La table utilisateur contient déja des éléments");
+		}
 
-    }
+	}
 }
--- EasyPermutWeb/src/etrs/selene/easypermut/beans/CreerPermutationPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/CreerPermutationPageBean.java
@@ -1,10 +1,13 @@
 package etrs.selene.easypermut.beans;
 
 import java.io.Serializable;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +15,13 @@ import lombok.experimental.FieldDefaults;
 import net.entetrs.commons.jsf.JsfUtils;
 import etrs.selene.easypermut.model.entities.DemandePermutation;
 import etrs.selene.easypermut.model.entities.Utilisateur;
+import etrs.selene.easypermut.model.entities.ZMR;
 import etrs.selene.easypermut.model.sessions.DemandePermutationSession;
+import etrs.selene.easypermut.model.sessions.ZMRSession;
 
 /**
  * Bean de la page de la creation d'une permutation.
- * 
+ *
  * @author SGT Mora Leo
  */
 @Named
@@ -24,74 +29,89 @@ import etrs.selene.easypermut.model.sessions.DemandePermutationSession;
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class CreerPermutationPageBean implements Serializable {
+public class CreerPermutationPageBean implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * {@link DemandePermutationSession}
+	 */
+	@Inject
+	DemandePermutationSession facadePermutations;
 
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * {@link DemandePermutationSession}
-     */
-    @Inject
-    DemandePermutationSession facadePermutations;
-
-    /**
-     * La permutation qui sera crée.
-     */
-    DemandePermutation permutation;
-
-    /**
-     * Utilisateur connecté.
-     */
-    Utilisateur utilisateur;
-
-    /**
-     * Methode de post-construction. Recupere l'utilisateur du FlashScope.
-     */
-    @PostConstruct
-    public void init() {
-        utilisateur = (Utilisateur) JsfUtils.getFromFlashScope("_utilisateur");
-        this.permutation = this.facadePermutations.newInstance();
-    }
-
-    /**
-     * Methude de mise en FlashScope de l'utilisateur.
-     *
-     * @param utilisateur
-     *            L'utilisateur a mettre dans le FlashScope.
-     */
-    public void flashUtilisateur(final Utilisateur utilisateur) {
-        JsfUtils.putInFlashScope("_utilisateur", utilisateur);
-    }
-
-    /**
-     * Methode de reation d'une permutation.
-     * 
-     * @return La page suivante.
-     */
-    public String creerDemande() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.permutation.setUtilisateurCreateur(utilisateur);
-        this.facadePermutations.create(permutation);
-        this.flashUtilisateur(utilisateur);
-        return "/accueil.xhtml";
-    }
-
-    // TODO Ajouter comments.
-    public String pageListePermutation() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.flashUtilisateur(this.utilisateur);
-        return "/listePermutations.xhtml";
-    }
-
-    public String pageAccueil() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.flashUtilisateur(this.utilisateur);
-        return "/accueil.xhtml";
-    }
+	/**
+	 * {@link ZMRSession}
+	 */
+	@Inject
+	ZMRSession facadeZmr;
+	
+	/**
+	 * La permutation qui sera crée.
+	 */
+	DemandePermutation permutation;
+	
+	/**
+	 * Utilisateur connecté.
+	 */
+	Utilisateur utilisateur;
+	
+	/**
+	 * Liste de zmr pour la checkliste zmr.
+	 */
+	List<ZMR> listeZMR;
+	
+	/**
+	 * Methode de post-construction. Recupere l'utilisateur du FlashScope.
+	 */
+	@PostConstruct
+	public void init()
+	{
+		this.utilisateur = (Utilisateur)JsfUtils.getFromFlashScope("_utilisateur");
+		this.permutation = this.facadePermutations.newInstance();
+		this.listeZMR = this.facadeZmr.readAll();
+	}
+	
+	/**
+	 * Methude de mise en FlashScope de l'utilisateur.
+	 *
+	 * @param utilisateur
+	 *            L'utilisateur a mettre dans le FlashScope.
+	 */
+	public void flashUtilisateur(final Utilisateur utilisateur)
+	{
+		JsfUtils.putInFlashScope("_utilisateur", utilisateur);
+	}
+	
+	/**
+	 * Methode de reation d'une permutation.
+	 *
+	 * @return La page suivante.
+	 */
+	public String creerDemande()
+	{
+		if (this.utilisateur == null)
+			return "/connexion.xhtml";
+		this.permutation.setUtilisateurCreateur(this.utilisateur);
+		this.facadePermutations.create(this.permutation);
+		this.flashUtilisateur(this.utilisateur);
+		return "/accueil.xhtml";
+	}
+	
+	// TODO Ajouter comments.
+	public String pageListePermutation()
+	{
+		if (this.utilisateur == null)
+			return "/connexion.xhtml";
+		this.flashUtilisateur(this.utilisateur);
+		return "/listePermutations.xhtml";
+	}
+	
+	public String pageAccueil()
+	{
+		if (this.utilisateur == null)
+			return "/connexion.xhtml";
+		this.flashUtilisateur(this.utilisateur);
+		return "/accueil.xhtml";
+	}
 }

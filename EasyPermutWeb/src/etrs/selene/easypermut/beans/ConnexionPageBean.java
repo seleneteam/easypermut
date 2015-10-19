31b62a34538a2603a978f150cc0ab4d3aaa6e599
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-19 09:13:20 +0200

    verification des données ok

--- EasyPermutWeb/src/etrs/selene/easypermut/beans/ConnexionPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/ConnexionPageBean.java
@@ -1,10 +1,12 @@
 package etrs.selene.easypermut.beans;
 
 import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,71 +25,83 @@ import etrs.selene.easypermut.model.sessions.UtilisateurSession;
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ConnexionPageBean implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * L'utilisateur dont l'identifiant anudef est present dans le formulaire.
-     */
-    Utilisateur utilisateur;
-
-    /**
-     * {@link UtilisateurSession}
-     */
-    @Inject
-    UtilisateurSession facadeUtilisateur;
-
-    /**
-     * Methode de post-construction. Instancie un nouvel utilisateur.
-     */
-    @PostConstruct
-    public void init() {
-        this.utilisateur = this.facadeUtilisateur.newInstance();
-    }
-
-    /**
-     * Methude de mise en FlashScope de l'utilisateur.
-     *
-     * @param utilisateur
-     *            L'utilisateur a mettre dans le FlashScope.
-     */
-    public void flashUtilisateur(final Utilisateur utilisateur) {
-        JsfUtils.putInFlashScope("_utilisateur", utilisateur);
-    }
-
-    /**
-     * Methode de connexion. Si l'utilisateur existe et qu'il est validé (voir
-     * {@link Utilisateur#getEstValide()}) redirige vers la page d'accueil de
-     * l'application. Si l'utilisateur existe, qu'il est valide mais qu'il n'a
-     * pas verifié ces informations, le redirige vers la page de validation des
-     * informations. Si l'utilisateur existe, mais qu'il n'est pas validé,
-     * redirige vers une page d'attente. Si l'utilisateur n'existe pas, l'ajoute
-     * dans la BDD et met son status a non valide.
-     *
-     * @return L'URL de redirection en fonction du cas.
-     */
-    public String connexion() {
-        String redirectedUrl;
-        Utilisateur utilisateurATester = this.facadeUtilisateur.searchFirstResult("identifiantAnudef", this.utilisateur.getIdentifiantAnudef().toLowerCase());
-
-        if (utilisateurATester != null && utilisateurATester.getEstValide() == true) {
-            if (utilisateurATester.getInformationsValide() == true) {
-                redirectedUrl = "./pages/accueil.xhtml";
-            } else {
-                redirectedUrl = "./pages/validation.xhtml";
-            }
-            this.flashUtilisateur(utilisateurATester);
-
-        } else if (utilisateurATester != null && utilisateurATester.getEstValide() == false) {
-            redirectedUrl = "./pages/validationEnCours.xhtml";
-        } else {
-            this.utilisateur.setIdentifiantAnudef(this.utilisateur.getIdentifiantAnudef().toLowerCase());
-            this.utilisateur.setEstValide(false);
-            this.facadeUtilisateur.create(this.utilisateur);
-            redirectedUrl = "./pages/validation.xhtml";
-        }
-
-        return redirectedUrl;
-    }
+public class ConnexionPageBean implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * L'utilisateur dont l'identifiant anudef est present dans le formulaire.
+	 */
+	Utilisateur utilisateur;
+	
+	/**
+	 * {@link UtilisateurSession}
+	 */
+	@Inject
+	UtilisateurSession facadeUtilisateur;
+	
+	/**
+	 * Methode de post-construction. Instancie un nouvel utilisateur.
+	 */
+	@PostConstruct
+	public void init()
+	{
+		this.utilisateur = this.facadeUtilisateur.newInstance();
+	}
+	
+	/**
+	 * Methude de mise en FlashScope de l'utilisateur.
+	 *
+	 * @param utilisateur
+	 *            L'utilisateur a mettre dans le FlashScope.
+	 */
+	public void flashUtilisateur(final Utilisateur utilisateur)
+	{
+		JsfUtils.putInFlashScope("_utilisateur", utilisateur);
+	}
+	
+	/**
+	 * Methode de connexion. Si l'utilisateur existe et qu'il est validé (voir
+	 * {@link Utilisateur#getEstValide()}) redirige vers la page d'accueil de
+	 * l'application. Si l'utilisateur existe, qu'il est valide mais qu'il n'a
+	 * pas verifié ces informations, le redirige vers la page de validation des
+	 * informations. Si l'utilisateur existe, mais qu'il n'est pas validé,
+	 * redirige vers une page d'attente. Si l'utilisateur n'existe pas, l'ajoute
+	 * dans la BDD et met son status a non valide.
+	 *
+	 * @return L'URL de redirection en fonction du cas.
+	 */
+	public String connexion()
+	{
+		String redirectedUrl;
+		Utilisateur utilisateurATester = this.facadeUtilisateur.searchFirstResult("identifiantAnudef", this.utilisateur.getIdentifiantAnudef().toLowerCase());
+		
+		if (utilisateurATester != null && utilisateurATester.getEstValide() == true)
+		{
+			if (utilisateurATester.getInformationsValide() == true)
+			{
+				redirectedUrl = "./pages/accueil.xhtml";
+			}
+			else
+			{
+				redirectedUrl = "./pages/verification.xhtml";
+			}
+			this.flashUtilisateur(utilisateurATester);
+			
+		}
+		else if (utilisateurATester != null && utilisateurATester.getEstValide() == false)
+		{
+			redirectedUrl = "./pages/validationEnCours.xhtml";
+		}
+		else
+		{
+			this.utilisateur.setIdentifiantAnudef(this.utilisateur.getIdentifiantAnudef().toLowerCase());
+			this.utilisateur.setEstValide(false);
+			this.facadeUtilisateur.create(this.utilisateur);
+			redirectedUrl = "./pages/validation.xhtml";
+		}
+		
+		return redirectedUrl;
+	}
 }
--- EasyPermutWeb/src/etrs/selene/easypermut/beans/VerifierDonneesPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/VerifierDonneesPageBean.java
@@ -1,10 +1,12 @@
 package etrs.selene.easypermut.beans;
 
 import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,54 +17,59 @@ import etrs.selene.easypermut.model.sessions.UtilisateurSession;
 
 /**
  * Bean de la page de verification des données.
- * 
- * @author SGT Mora Leo
  *
+ * @author SGT Mora Leo
  */
 @Named
 @ViewScoped
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class VerifierDonneesPageBean implements Serializable {
+public class VerifierDonneesPageBean implements Serializable
+{
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Inject
-    UtilisateurSession facadeUtilisateur;
+	@Inject
+	UtilisateurSession facadeUtilisateur;
 
-    /**
-     * Utilisateur connecté.
-     */
-    Utilisateur utilisateur;
+	/**
+	 * Utilisateur connecté.
+	 */
+	Utilisateur utilisateur;
 
-    @PostConstruct
-    public void init() {
-        this.utilisateur = (Utilisateur) JsfUtils.getFromFlashScope("_utilisateur");
-    }
+	@PostConstruct
+	public void init()
+	{
+		this.utilisateur = (Utilisateur)JsfUtils.getFromFlashScope("_utilisateur");
+	}
 
-    /**
-     * Methude de mise en FlashScope de l'utilisateur.
-     *
-     * @param utilisateur
-     *            L'utilisateur a mettre dans le FlashScope.
-     */
-    public void flashUtilisateur(final Utilisateur utilisateur) {
-        JsfUtils.putInFlashScope("_utilisateur", utilisateur);
-    }
+	/**
+	 * Methude de mise en FlashScope de l'utilisateur.
+	 *
+	 * @param utilisateur
+	 *            L'utilisateur a mettre dans le FlashScope.
+	 */
+	public void flashUtilisateur(final Utilisateur utilisateur)
+	{
+		JsfUtils.putInFlashScope("_utilisateur", utilisateur);
+	}
 
-    /**
-     * Methode de sauvegarde des nouvelles données rentrées par l'utilisateur.
-     * 
-     * @return La page suivante.
-     */
-    public String sauvegarderDonnees() {
-        if (utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.utilisateur.setInformationsValide(true);
-        this.facadeUtilisateur.update(utilisateur);
-        this.flashUtilisateur(utilisateur);
-        return "/accueil.xhtml";
-    }
+	/**
+	 * Methode de sauvegarde des nouvelles données rentrées par l'utilisateur.
+	 *
+	 * @return La page suivante.
+	 */
+	public String sauvegarderDonnees()
+	{
+
+		if (this.utilisateur == null)
+			return "/connexion.xhtml";
+		System.out.println(this.utilisateur.getNom());
+		this.utilisateur.setInformationsValide(true);
+		this.facadeUtilisateur.update(this.utilisateur);
+		this.flashUtilisateur(this.utilisateur);
+
+		return "/pages/accueil.xhtml";
+	}
 }

60f5e9f67fbb3c5b30f6d6141d046133fe0c0718
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-19 09:35:15 +0200

    ajout pre-stat sur accueil

--- EasyPermutWeb/src/etrs/selene/easypermut/beans/AccueilPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/AccueilPageBean.java
@@ -1,10 +1,12 @@
 package etrs.selene.easypermut.beans;
 
 import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,64 +25,45 @@ import etrs.selene.easypermut.model.sessions.UtilisateurSession;
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class AccueilPageBean implements Serializable {
+public class AccueilPageBean implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * L'utilisateur dont l'identifiant anudef est present dans le formulaire.
+	 */
+	Utilisateur utilisateur;
+	
+	/**
+	 * {@link UtilisateurSession}
+	 */
+	@Inject
+	UtilisateurSession facadeUtilisateur;
+	
+	/**
+	 * Methode de post-construction. Instancie un nouvel utilisateur.
+	 */
+	@PostConstruct
+	public void init()
+	{
+		this.utilisateur = this.facadeUtilisateur.newInstance();
+	}
+	
+	/**
+	 * Methude de mise en FlashScope de l'utilisateur.
+	 *
+	 * @param utilisateur
+	 *            L'utilisateur a mettre dans le FlashScope.
+	 */
+	public void flashUtilisateur(final Utilisateur utilisateur)
+	{
+		JsfUtils.putInFlashScope("_utilisateur", utilisateur);
+	}
 
-    // TODO Supprimer Page
-
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * L'utilisateur dont l'identifiant anudef est present dans le formulaire.
-     */
-    Utilisateur utilisateur;
-
-    /**
-     * {@link UtilisateurSession}
-     */
-    @Inject
-    UtilisateurSession facadeUtilisateur;
-
-    /**
-     * Methode de post-construction. Instancie un nouvel utilisateur.
-     */
-    @PostConstruct
-    public void init() {
-        this.utilisateur = this.facadeUtilisateur.newInstance();
-    }
-
-    /**
-     * Methude de mise en FlashScope de l'utilisateur.
-     *
-     * @param utilisateur
-     *            L'utilisateur a mettre dans le FlashScope.
-     */
-    public void flashUtilisateur(final Utilisateur utilisateur) {
-        JsfUtils.putInFlashScope("_utilisateur", utilisateur);
-    }
-
-    /**
-     * Navigation entre les pages.
-     * 
-     * @return La page a ateindre en fonction du cas.
-     */
-    public String pageCreerPermutation() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.flashUtilisateur(this.utilisateur);
-        return "/creationPermutation.xhtml";
-    }
-
-    /**
-     * Navigation entre les pages.
-     * 
-     * @return La page a ateindre en fonction du cas.
-     */
-    public String pageListePermutation() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.flashUtilisateur(this.utilisateur);
-        return "/listePermutations.xhtml";
-    }
+	public Long nombreInscrits()
+	{
+		return this.facadeUtilisateur.count();
+	}
+	
 }
--- EasyPermutWeb/src/etrs/selene/easypermut/beans/ListerPermutationsPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/ListerPermutationsPageBean.java
@@ -3,10 +3,12 @@ package etrs.selene.easypermut.beans;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +21,7 @@ import etrs.selene.easypermut.model.sessions.UtilisateurSession;
 
 /**
  * Bean de la page d'affichage des permutations.
- * 
+ *
  * @author SGT Mora Leo
  */
 @Named
@@ -27,156 +29,149 @@ import etrs.selene.easypermut.model.sessions.UtilisateurSession;
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ListerPermutationsPageBean implements Serializable {
+public class ListerPermutationsPageBean implements Serializable
+{
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * {@link DemandePermutationSession}
-     */
-    @Inject
-    DemandePermutationSession facadePermutations;
+	/**
+	 * {@link DemandePermutationSession}
+	 */
+	@Inject
+	DemandePermutationSession facadePermutations;
 
-    /**
-     * {@link UtilisateurSession}
-     */
-    @Inject
-    UtilisateurSession facadeUtilisateur;
+	/**
+	 * {@link UtilisateurSession}
+	 */
+	@Inject
+	UtilisateurSession facadeUtilisateur;
 
-    /**
-     * Utilisateur connecté.
-     */
-    Utilisateur utilisateur;
+	/**
+	 * Utilisateur connecté.
+	 */
+	Utilisateur utilisateur;
 
-    /**
-     * La demannde de permutation selectionée pour les details et pour
-     * l'acceptation.
-     */
-    DemandePermutation demandePermutationSelectionee;
+	/**
+	 * La demannde de permutation selectionée pour les details et pour
+	 * l'acceptation.
+	 */
+	DemandePermutation demandePermutationSelectionee;
 
-    /**
-     * Methode de post-construction. Recupere l'utilisateur du FlashScope.
-     */
-    @PostConstruct
-    public void init() {
-        this.utilisateur = (Utilisateur) JsfUtils.getFromFlashScope("_utilisateur");
-        this.demandePermutationSelectionee = this.facadePermutations.newInstance();
-    }
+	/**
+	 * Methode de post-construction. Recupere l'utilisateur du FlashScope.
+	 */
+	@PostConstruct
+	public void init()
+	{
+		this.utilisateur = (Utilisateur)JsfUtils.getFromFlashScope("_utilisateur");
+		this.demandePermutationSelectionee = this.facadePermutations.newInstance();
+	}
 
-    /**
-     * Methude de mise en FlashScope de l'utilisateur.
-     *
-     * @param utilisateur
-     *            L'utilisateur a mettre dans le FlashScope.
-     */
-    public void flashUtilisateur(final Utilisateur utilisateur) {
-        JsfUtils.putInFlashScope("_utilisateur", utilisateur);
-    }
+	/**
+	 * Methude de mise en FlashScope de l'utilisateur.
+	 *
+	 * @param utilisateur
+	 *            L'utilisateur a mettre dans le FlashScope.
+	 */
+	public void flashUtilisateur(final Utilisateur utilisateur)
+	{
+		JsfUtils.putInFlashScope("_utilisateur", utilisateur);
+	}
 
-    /**
-     * Retourne la liste de toutes les permutations.
-     * 
-     * @return Une liste de permutation.
-     */
-    public List<DemandePermutation> getLstDemandesPermutation() {
-        return this.facadePermutations.readAll();
-    }
+	/**
+	 * Retourne la liste de toutes les permutations.
+	 *
+	 * @return Une liste de permutation.
+	 */
+	public List<DemandePermutation> getLstDemandesPermutation()
+	{
+		return this.facadePermutations.readAll();
+	}
 
-    /**
-     * Retourne la liste des permutations sans celles en cours de transaction.
-     * 
-     * @return Une liste de permutation valide.
-     */
-    public List<DemandePermutation> getLstDemandesPermutationValides() {
-        List<DemandePermutation> lstPermutValides = new ArrayList<>();
+	/**
+	 * Retourne la liste des permutations sans celles en cours de transaction.
+	 *
+	 * @return Une liste de permutation valide.
+	 */
+	public List<DemandePermutation> getLstDemandesPermutationValides()
+	{
+		List<DemandePermutation> lstPermutValides = new ArrayList<>();
 
-        for (DemandePermutation demandePermutation : this.facadePermutations.readAll()) {
-            if (demandePermutation.getUtilisateurInteresse() == null) {
-                lstPermutValides.add(demandePermutation);
-            }
-        }
-        return lstPermutValides;
-    }
+		for (DemandePermutation demandePermutation : this.facadePermutations.readAll())
+		{
+			if (demandePermutation.getUtilisateurInteresse() == null)
+			{
+				lstPermutValides.add(demandePermutation);
+			}
+		}
+		return lstPermutValides;
+	}
 
-    /**
-     * Retourne la liste des permutations compatibles avec l'utilisateur. Donc,
-     * de meme grade et de meme spécilité.
-     * 
-     * @return Une liste de permutation.
-     */
-    public List<DemandePermutation> getLstDemandesPermutationSpecifiques() {
-        if (this.utilisateur.getEstInteresse() == false) {
-            List<DemandePermutation> lstPermutSpecifique = new ArrayList<>();
-            List<DemandePermutation> lstPermut = this.facadePermutations.search("specilite", utilisateur.getSpecialite());
+	/**
+	 * Retourne la liste des permutations compatibles avec l'utilisateur. Donc,
+	 * de meme grade et de meme spécilité.
+	 *
+	 * @return Une liste de permutation.
+	 */
+	public List<DemandePermutation> getLstDemandesPermutationSpecifiques()
+	{
+		if (this.utilisateur.getEstInteresse() == false)
+		{
+			List<DemandePermutation> lstPermutSpecifique = new ArrayList<>();
+			List<DemandePermutation> lstPermut = this.facadePermutations.search("specilite", this.utilisateur.getSpecialite());
 
-            for (DemandePermutation demandePermutation : lstPermut) {
-                if (demandePermutation.getUtilisateurCreateur().getGrade() == this.utilisateur.getGrade()) {
-                    if (demandePermutation.getUtilisateurInteresse() == null) {
-                        lstPermutSpecifique.add(demandePermutation);
-                    }
-                }
-            }
-            return lstPermutSpecifique;
-        } else {
-            return null;
-        }
-    }
+			for (DemandePermutation demandePermutation : lstPermut)
+			{
+				if (demandePermutation.getUtilisateurCreateur().getGrade() == this.utilisateur.getGrade())
+				{
+					if (demandePermutation.getUtilisateurInteresse() == null)
+					{
+						lstPermutSpecifique.add(demandePermutation);
+					}
+				}
+			}
+			return lstPermutSpecifique;
+		}
+		else
+			return null;
+	}
 
-    /**
-     * Methode de choix d'une permutation. Ajoute l'utilisateur a la
-     * permutation.
-     * 
-     * @param permutation
-     * @return La page suivante.
-     */
-    public String choisirPermutation() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        if (this.utilisateur.getEstInteresse() == false) {
-            this.utilisateur.setEstInteresse(true);
-            this.demandePermutationSelectionee.setUtilisateurInteresse(this.utilisateur);
-            this.facadePermutations.update(this.demandePermutationSelectionee);
-            this.facadeUtilisateur.update(this.utilisateur);
-            JsfUtils.sendGrowlMessage("Vous avez choisi la permutation de %s", this.demandePermutationSelectionee.getUtilisateurCreateur().getNom());
-        }
-        return "/pages/accueil.xhtml";
-    }
+	/**
+	 * Methode de choix d'une permutation. Ajoute l'utilisateur a la
+	 * permutation.
+	 *
+	 * @param permutation
+	 * @return La page suivante.
+	 */
+	public String choisirPermutation()
+	{
+		if (this.utilisateur == null)
+			return "/connexion.xhtml";
+		if (this.utilisateur.getEstInteresse() == false)
+		{
+			this.utilisateur.setEstInteresse(true);
+			this.demandePermutationSelectionee.setUtilisateurInteresse(this.utilisateur);
+			this.facadePermutations.update(this.demandePermutationSelectionee);
+			this.facadeUtilisateur.update(this.utilisateur);
+			JsfUtils.sendGrowlMessage("Vous avez choisi la permutation de %s", this.demandePermutationSelectionee.getUtilisateurCreateur().getNom());
+		}
+		return "/pages/accueil.xhtml";
+	}
 
-    /**
-     * Navigation entre les pages.
-     * 
-     * @return La page a ateindre en fonction du cas.
-     */
-    public String pageCreerPermutation() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.flashUtilisateur(this.utilisateur);
-        return "/pages/creationPermutation.xhtml";
-    }
+	/**
+	 * Sélectionne une ermutation pour les details et l'acceptation.
+	 *
+	 * @param permutation
+	 *            La permutation.
+	 */
+	public void selectionerPermutation(final DemandePermutation permutation)
+	{
+		this.demandePermutationSelectionee = permutation;
+	}
 
-    /**
-     * Navigation entre les pages.
-     * 
-     * @return La page a ateindre en fonction du cas.
-     */
-    public String pageAccueil() {
-        if (this.utilisateur == null) {
-            return "/connexion.xhtml";
-        }
-        this.flashUtilisateur(this.utilisateur);
-        return "/accueil.xhtml";
-    }
-
-    /**
-     * Sélectionne une ermutation pour les details et l'acceptation.
-     * 
-     * @param permutation
-     *            La permutation.
-     */
-    public void selectionerPermutation(DemandePermutation permutation) {
-        this.demandePermutationSelectionee = permutation;
-    }
+	public long nombreDemandes()
+	{
+		return this.facadePermutations.count();
+	}
 
 }

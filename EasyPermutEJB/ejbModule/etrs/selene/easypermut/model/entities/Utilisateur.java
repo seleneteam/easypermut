1c2469515e29f4a13f3bddb43cfba87bb6452f8f
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-10-21 16:38:50 +0200

    changement requete pour graph

--- EasyPermutEJB/ejbModule/etrs/selene/easypermut/model/entities/Utilisateur.java
+++ EasyPermutEJB/ejbModule/etrs/selene/easypermut/model/entities/Utilisateur.java
@@ -4,17 +4,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
+
 import etrs.selene.easypermut.model.commons.AbstractEntity;
 
 /**
@@ -26,112 +30,116 @@ import etrs.selene.easypermut.model.commons.AbstractEntity;
 @NoArgsConstructor
 @Entity
 @Table(name = "UTILISATEUR")
-@NamedQuery(name = "Utilisateur.qtParGrade", query = "SELECT COUNT(u) FROM Utilisateur u WHERE u.grade = :grade")
-public class Utilisateur extends AbstractEntity {
+@NamedQueries({ @NamedQuery(name = "Utilisateur.qtParGrade",
+query = "SELECT COUNT(u) FROM Utilisateur u WHERE u.grade = :grade AND u.estValide = 1"), @NamedQuery(name = "Utilisateur.qtTotal",
+query = "SELECT COUNT(u) FROM Utilisateur u WHERE u.estValide = 1") })
+public class Utilisateur extends AbstractEntity
+{
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Identifiant Anudef de l'utilisateur.
-     */
-    @Column(name = "identifiant_anudef")
-    @Length(min = 1)
-    @NotNull
-    @Getter
-    @Setter
-    String identifiantAnudef;
+	/**
+	 * Identifiant Anudef de l'utilisateur.
+	 */
+	@Column(name = "identifiant_anudef")
+	@Length(min = 1)
+	@NotNull
+	@Getter
+	@Setter
+	String identifiantAnudef;
 
-    /**
-     * Nom de l'utilisateur.
-     */
-    @Column(name = "nom")
-    @Getter
-    @Setter
-    String nom;
+	/**
+	 * Nom de l'utilisateur.
+	 */
+	@Column(name = "nom")
+	@Getter
+	@Setter
+	String nom;
 
-    /**
-     * Prenom de l'utiisateur.
-     */
-    @Column(name = "prenom")
-    @Getter
-    @Setter
-    String prenom;
+	/**
+	 * Prenom de l'utiisateur.
+	 */
+	@Column(name = "prenom")
+	@Getter
+	@Setter
+	String prenom;
 
-    /**
-     * NIA de l'utilisateur.
-     */
-    @Column(name = "nia")
-    @Getter
-    @Setter
-    String nia;
+	/**
+	 * NIA de l'utilisateur.
+	 */
+	@Column(name = "nia")
+	@Getter
+	@Setter
+	String nia;
 
-    /**
-     * Mail de l'utilisateur.
-     */
-    @Column(name = "mail")
-    @Email
-    @Getter
-    @Setter
-    String mail;
+	/**
+	 * Mail de l'utilisateur.
+	 */
+	@Column(name = "mail")
+	@Email
+	@Getter
+	@Setter
+	String mail;
 
-    /**
-     * Grade de l'utilisateur.
-     */
-    @ManyToOne
-    @JoinColumn(name = "grade_id")
-    @Getter
-    @Setter
-    Grade grade;
+	/**
+	 * Grade de l'utilisateur.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "grade_id")
+	@Getter
+	@Setter
+	Grade grade;
 
-    /**
-     * Specialite de l'utilisateur.
-     */
-    @ManyToOne
-    @JoinColumn(name = "specialite_id")
-    @Getter
-    @Setter
-    Specialite specialite;
+	/**
+	 * Specialite de l'utilisateur.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "specialite_id")
+	@Getter
+	@Setter
+	Specialite specialite;
 
-    /**
-     * Poste occupé par l'utilisateur.
-     */
-    @OneToOne
-    @JoinColumn(name = "poste_id")
-    @Getter
-    @Setter
-    Poste poste;
+	/**
+	 * Poste occupé par l'utilisateur.
+	 */
+	@OneToOne
+	@JoinColumn(name = "poste_id")
+	@Getter
+	@Setter
+	Poste poste;
 
-    /**
-     * Definit si l'utilisateur peut se connecter a l'application ou non. false
-     * si les données de l'utilisateur on été importées. true si il doit
-     * attendre l'importation de ces données.
-     */
-    @Column(name = "valide")
-    @Getter
-    @Setter
-    Boolean estValide;
+	/**
+	 * Definit si l'utilisateur peut se connecter a l'application ou non. false
+	 * si les données de l'utilisateur on été importées. true si il doit
+	 * attendre l'importation de ces données.
+	 */
+	@Column(name = "valide")
+	@Getter
+	@Setter
+	Boolean estValide;
 
-    /**
-     * Definit si l'utilisateur est deja en cours de transaction pour une
-     * permutation.
-     */
-    @Column(name = "interesse")
-    @Getter
-    @Setter
-    Boolean estInteresse;
+	/**
+	 * Definit si l'utilisateur est deja en cours de transaction pour une
+	 * permutation.
+	 */
+	@Column(name = "interesse")
+	@Getter
+	@Setter
+	Boolean estInteresse;
 
-    /**
-     * Definit si l'utilisateur à validé ses informations.
-     */
-    @Column(name = "informations_valides")
-    @Getter
-    @Setter
-    Boolean informationsValide;
+	/**
+	 * Definit si l'utilisateur à validé ses informations.
+	 */
+	@Column(name = "informations_valides")
+	@Getter
+	@Setter
+	Boolean informationsValide;
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.nom);
-        return sb.toString();
-    }
+	@Override
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.nom);
+		return sb.toString();
+	}
 }
--- EasyPermutWeb/src/etrs/selene/easypermut/beans/StatsPageBean.java
+++ EasyPermutWeb/src/etrs/selene/easypermut/beans/StatsPageBean.java
@@ -1,19 +1,23 @@
 package etrs.selene.easypermut.beans;
 
 import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.PieChartModel;
+
 import etrs.selene.easypermut.model.entities.Grade;
 import etrs.selene.easypermut.model.entities.Utilisateur;
 import etrs.selene.easypermut.model.entities.ZMR;
@@ -27,136 +31,145 @@ import etrs.selene.easypermut.model.sessions.ZMRSession;
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class StatsPageBean implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * {@link UtilisateurSession}
-     */
-    @Inject
-    UtilisateurSession facadeUtilisateur;
-
-    /**
-     * {@link DemandePermutationSession}
-     */
-    @Inject
-    DemandePermutationSession facadeDemande;
-
-    /**
-     * {@link GradeSession}
-     */
-    @Inject
-    GradeSession facadeGrade;
-
-    /**
-     * {@link ZMRSession}
-     */
-    @Inject
-    ZMRSession facadeZMR;
-
-    /**
-     * {@link UtilisateurSessionBean}
-     */
-    @Inject
-    UtilisateurSessionBean sessionUtilisateur;
-
-    /**
-     * Utilisateur consultant la page.
-     */
-    Utilisateur utilisateur;
-
-    /**
-     * Graphique des utilisateurs en fonction des grades.
-     */
-    BarChartModel graphUtilisateurs;
-
-    /**
-     * Graphique des demandes en fonction des ZMR.
-     */
-    PieChartModel graphDemandes;
-
-    /**
-     * Méthode de post-construction. Récupère l'utilisateur flashé et cré les
-     * graphiques.
-     */
-    @PostConstruct
-    public void init() {
-        this.utilisateur = this.sessionUtilisateur.getUtilisateur();
-        this.createGraphUtilisateurs();
-        this.createGraphDemandes();
-    }
-
-    /**
-     * Retourne le nombre d'utilisateurs.
-     *
-     * @return le nombre d'utilisateurs.
-     */
-    public long nombreUtilisateurs() {
-        return this.facadeUtilisateur.count();
-    }
-
-    /**
-     * Retourne le nombre de demandes de permuations.
-     *
-     * @return le nombre de demandes.
-     */
-    public long nombreDemandes() {
-        return this.facadeDemande.count();
-    }
-
-    /**
-     * Cré le grahique des utilisateurs par grade.
-     */
-    private void createGraphUtilisateurs() {
-        this.graphUtilisateurs = this.initBarModel();
-
-        this.graphUtilisateurs.setTitle("Nombre d'utilisateurs par grade");
-        this.graphUtilisateurs.setLegendPosition("ne");
-
-        Axis xAxis = this.graphUtilisateurs.getAxis(AxisType.X);
-        xAxis.setLabel("Grade");
-
-        Axis yAxis = this.graphUtilisateurs.getAxis(AxisType.Y);
-        yAxis.setLabel("Utilisateurs");
-        yAxis.setMin(0);
-        yAxis.setMax(this.facadeUtilisateur.count());
-    }
-
-    /**
-     * Remplit le grahique des utilisateurs par grade.
-     *
-     * @return
-     */
-    private BarChartModel initBarModel() {
-        BarChartModel model = new BarChartModel();
-
-        ChartSeries utilisateurs = new ChartSeries();
-        utilisateurs.setLabel("Utilisateurs");
-
-        for (Grade grade : this.facadeGrade.readAll()) {
-            utilisateurs.set(grade.getGrade(), this.facadeUtilisateur.quantiteUtilisateurParGrade(grade));
-        }
-        model.addSeries(utilisateurs);
-
-        return model;
-
-    }
-
-    /**
-     * Cré et remplit de graphique des demandes par ZMR.
-     */
-    private void createGraphDemandes() {
-        this.graphDemandes = new PieChartModel();
-
-        for (ZMR zmr : this.facadeZMR.readAll()) {
-            this.graphDemandes.set(zmr.getLibelle(), this.facadeDemande.quantiteDemandesParZMR(zmr));
-        }
-
-        this.graphDemandes.setTitle("Nombre de demandes par ZMR");
-        this.graphDemandes.setLegendPosition("e");
-        this.graphDemandes.setFill(true);
-        this.graphDemandes.setShowDataLabels(true);
-        this.graphDemandes.setDiameter(300);
-    }
+public class StatsPageBean implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * {@link UtilisateurSession}
+	 */
+	@Inject
+	UtilisateurSession facadeUtilisateur;
+	
+	/**
+	 * {@link DemandePermutationSession}
+	 */
+	@Inject
+	DemandePermutationSession facadeDemande;
+	
+	/**
+	 * {@link GradeSession}
+	 */
+	@Inject
+	GradeSession facadeGrade;
+	
+	/**
+	 * {@link ZMRSession}
+	 */
+	@Inject
+	ZMRSession facadeZMR;
+	
+	/**
+	 * {@link UtilisateurSessionBean}
+	 */
+	@Inject
+	UtilisateurSessionBean sessionUtilisateur;
+	
+	/**
+	 * Utilisateur consultant la page.
+	 */
+	Utilisateur utilisateur;
+	
+	/**
+	 * Graphique des utilisateurs en fonction des grades.
+	 */
+	BarChartModel graphUtilisateurs;
+	
+	/**
+	 * Graphique des demandes en fonction des ZMR.
+	 */
+	PieChartModel graphDemandes;
+	
+	/**
+	 * Méthode de post-construction. Récupère l'utilisateur flashé et cré les
+	 * graphiques.
+	 */
+	@PostConstruct
+	public void init()
+	{
+		this.utilisateur = this.sessionUtilisateur.getUtilisateur();
+		this.createGraphUtilisateurs();
+		this.createGraphDemandes();
+	}
+	
+	/**
+	 * Retourne le nombre d'utilisateurs.
+	 *
+	 * @return le nombre d'utilisateurs.
+	 */
+	public long nombreUtilisateurs()
+	{
+		return this.facadeUtilisateur.count();
+	}
+	
+	/**
+	 * Retourne le nombre de demandes de permuations.
+	 *
+	 * @return le nombre de demandes.
+	 */
+	public long nombreDemandes()
+	{
+		return this.facadeDemande.count();
+	}
+	
+	/**
+	 * Cré le grahique des utilisateurs par grade.
+	 */
+	private void createGraphUtilisateurs()
+	{
+		this.graphUtilisateurs = this.initBarModel();
+		
+		this.graphUtilisateurs.setTitle("Nombre d'utilisateurs par grade");
+		this.graphUtilisateurs.setLegendPosition("ne");
+		
+		Axis xAxis = this.graphUtilisateurs.getAxis(AxisType.X);
+		xAxis.setLabel("Grade");
+		
+		Axis yAxis = this.graphUtilisateurs.getAxis(AxisType.Y);
+		yAxis.setLabel("Utilisateurs");
+		yAxis.setMin(0);
+		yAxis.setMax(this.facadeUtilisateur.quantiteUtilisateurTotal());
+	}
+	
+	/**
+	 * Remplit le grahique des utilisateurs par grade.
+	 *
+	 * @return
+	 */
+	private BarChartModel initBarModel()
+	{
+		BarChartModel model = new BarChartModel();
+		
+		ChartSeries utilisateurs = new ChartSeries();
+		utilisateurs.setLabel("Utilisateurs");
+		
+		for (Grade grade : this.facadeGrade.readAll())
+		{
+			utilisateurs.set(grade.getGrade(), this.facadeUtilisateur.quantiteUtilisateurParGrade(grade));
+		}
+		model.addSeries(utilisateurs);
+		
+		return model;
+		
+	}
+	
+	/**
+	 * Cré et remplit de graphique des demandes par ZMR.
+	 */
+	private void createGraphDemandes()
+	{
+		this.graphDemandes = new PieChartModel();
+		
+		for (ZMR zmr : this.facadeZMR.readAll())
+		{
+			this.graphDemandes.set(zmr.getLibelle(), this.facadeDemande.quantiteDemandesParZMR(zmr));
+		}
+		
+		this.graphDemandes.setTitle("Nombre de demandes par ZMR");
+		this.graphDemandes.setLegendPosition("e");
+		this.graphDemandes.setFill(true);
+		this.graphDemandes.setShowDataLabels(true);
+		this.graphDemandes.setDiameter(300);
+	}
 }

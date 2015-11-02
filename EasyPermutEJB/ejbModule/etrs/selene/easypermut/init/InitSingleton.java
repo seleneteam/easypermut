bfabb6eb5e0b59e306d8f1d27732d52e5be6ceb5
louis-marie Merminod <lm.merminod@yahoo.fr>
2015-11-02 09:38:33 +0100

    ajout du caroussel

--- EasyPermutEJB/ejbModule/etrs/selene/easypermut/init/InitSingleton.java
+++ EasyPermutEJB/ejbModule/etrs/selene/easypermut/init/InitSingleton.java
@@ -34,49 +34,49 @@ import etrs.selene.easypermut.model.sessions.ZMRSession;
  */
 public class InitSingleton
 {
-
+	
 	/**
 	 * {@link GradeSession}
 	 */
 	@Inject
 	private GradeSession facadeGrade;
-
+	
 	/**
 	 * {@link PosteSession}
 	 */
 	@Inject
 	private PosteSession facadePoste;
-
+	
 	/**
 	 * {@link SpecialiteSession}
 	 */
 	@Inject
 	private SpecialiteSession facadeSpecialite;
-
+	
 	/**
 	 * {@link UniteSession}
 	 */
 	@Inject
 	private UniteSession facadeUnite;
-
+	
 	/**
 	 * {@link UtilisateurSession}
 	 */
 	@Inject
 	private UtilisateurSession facadeUtilisateur;
-
+	
 	/**
 	 * {@link VilleSession}
 	 */
 	@Inject
 	private VilleSession facadeVille;
-
+	
 	/**
 	 * {@link ZMRSession}
 	 */
 	@Inject
 	private ZMRSession facadeZMR;
-
+	
 	String[] tabGrade = { "Caporal", "Caporal-Chef", "Sergent", "Adjudant" };
 	String[] tabPoste = { "Mecanicien", "Electrotechnichien", "Previsionniste" };
 	String[] tabNomSpecialite = { "Programmeur", "Admin-Reseaux", "Communication" };
@@ -85,7 +85,7 @@ public class InitSingleton
 	String[] tabUtilisateur = { "Didier", "Serge", "Cecile" };
 	String[] tabVille = { "Paris", "Rennes", "Bordeaux" };
 	String[] tabZMR = { "Creil", "Angers-Rennes", "Bordeaux-Cazaux" };
-
+	
 	@PostConstruct
 	/**
 	 * Methode d'initialisation de l'application.
@@ -93,7 +93,7 @@ public class InitSingleton
 	public void init()
 	{
 		log.info("Vérification de la base de donnée.....");
-
+		
 		/*
 		 * Ajout de grades.
 		 */
@@ -111,13 +111,13 @@ public class InitSingleton
 		{
 			log.info("La table grade contient déja des éléments");
 		}
-
+		
 		/*
 		 * Ajout de ZMR.
 		 */
 		if (this.facadeZMR.readAll().isEmpty())
 		{
-
+			
 			for (String string : this.tabZMR)
 			{
 				ZMR zmr = this.facadeZMR.newInstance();
@@ -130,14 +130,14 @@ public class InitSingleton
 		{
 			log.info("La table ville contient déja des éléments");
 		}
-
+		
 		/*
 		 * Ajout de villes.
 		 */
 		if (this.facadeVille.readAll().isEmpty())
 		{
 			int index = 0;
-
+			
 			for (String string : this.tabVille)
 			{
 				Ville ville = this.facadeVille.newInstance();
@@ -152,14 +152,14 @@ public class InitSingleton
 		{
 			log.info("La table ville contient déja des éléments");
 		}
-
+		
 		/*
 		 * Ajout d'unités.
 		 */
 		if (this.facadeUnite.readAll().isEmpty())
 		{
 			int index = 0;
-
+			
 			for (String string : this.tabUnite)
 			{
 				Unite unite = this.facadeUnite.newInstance();
@@ -174,14 +174,14 @@ public class InitSingleton
 		{
 			log.info("La table unite contient déja des éléments");
 		}
-
+		
 		/*
 		 * Ajout de postes.
 		 */
 		if (this.facadePoste.readAll().isEmpty())
 		{
 			int index = 0;
-
+			
 			for (String string : this.tabPoste)
 			{
 				Poste poste = this.facadePoste.newInstance();
@@ -196,14 +196,14 @@ public class InitSingleton
 		{
 			log.info("La table poste contient déja des éléments");
 		}
-
+		
 		/*
-		 * Ajout de specialite.
+		 * Ajout de specialités.
 		 */
 		if (this.facadeSpecialite.readAll().isEmpty())
 		{
 			int index = 0;
-
+			
 			for (String string : this.tabNomSpecialite)
 			{
 				Specialite specialite = this.facadeSpecialite.newInstance();
@@ -216,16 +216,16 @@ public class InitSingleton
 		}
 		else
 		{
-			log.info("La table specialite contient déja des éléments");
+			log.info("La table specialité contient déja des éléments");
 		}
-
+		
 		/*
 		 * Ajout d'utilisateurs.
 		 */
 		if (this.facadeUtilisateur.readAll().isEmpty())
 		{
 			int index = 0;
-
+			
 			Utilisateur utilisateur = this.facadeUtilisateur.newInstance();
 			utilisateur.setEstValide(true);
 			utilisateur.setEstInteresse(false);
@@ -242,7 +242,7 @@ public class InitSingleton
 			this.facadeUtilisateur.create(utilisateur);
 			log.info("Utilisateur " + this.tabUtilisateur[index] + " créé");
 			index++;
-
+			
 			Utilisateur utilisateur4 = this.facadeUtilisateur.newInstance();
 			utilisateur4.setEstValide(false);
 			utilisateur4.setEstInteresse(false);
@@ -251,7 +251,7 @@ public class InitSingleton
 			utilisateur4.setDateInscription(new Date());
 			this.facadeUtilisateur.create(utilisateur4);
 			log.info("Utilisateur 4 créé");
-
+			
 			Utilisateur utilisateur5 = this.facadeUtilisateur.newInstance();
 			utilisateur5.setEstValide(false);
 			utilisateur5.setInformationsValide(true);
@@ -260,7 +260,7 @@ public class InitSingleton
 			utilisateur5.setDateInscription(new Date());
 			this.facadeUtilisateur.create(utilisateur5);
 			log.info("Utilisateur 5 créé");
-
+			
 		}
 		else
 		{
